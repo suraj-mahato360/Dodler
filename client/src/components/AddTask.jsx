97d540bfc42a71553ff82b4cfb648e07f65d6453
Suraj Mahato <suryamahto081@gmail.com>
2023-05-14 22:28:33 +0530

    first commit

---
+++ client/src/components/AddTask.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { createTodo } from "../features/todos/todoSlice";
+
+const AddTask = () => {
+  
+  const [text, setText] = useState("");
+
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      alert("Please enter a text");
+    }
+    setText("");
+
+    dispatch(createTodo({ text }));
+  };
+
+  return (
+    <section className="form ml-72 mr-4 bg-gray-950 mt-24 lg:ml-4 lg:mt-24">
+      <form onSubmit={onSubmit} className="p-4">
+        <div className="form-group mb-4 bg-gray-700 w-full flex justify-center items-center">
+          <input
+            type="text"
+            id="text"
+            name="text"
+            className="px-4 py-4 text-xl border-[0] rounded w-full focus:outline-none bg-gray-700"
+            placeholder="Add a task"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="form-group flex justify-end">
+          <button className="btn btn-block py-2 px-4 bg-purple-500 text-white font-bold ml-2 rounded grid items-end" type="submit">
+            Add
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AddTask;
---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const port = process.env.PORT || 8000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+const todoRoutes = require("./routes/todoRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+app.use("/api/todos", todoRoutes);
+app.use("/api/users", userRoutes);
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "dist", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
